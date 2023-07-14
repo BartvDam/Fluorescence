daa308f3e1b738790707a9fe7f12e727f20aea09
BartvanDam <b.vdam@protonmai.com>
2023-07-14 19:21:09 +0200

    Single exponential fitting algorithm

--- decay/lifetime_fitting.py
+++ decay/lifetime_fitting.py
@@ -1,6 +1,3 @@
-#%% test2
-
-print('test')
 #%% libraries
 
 import numpy as np
@@ -23,43 +20,48 @@ def convoluted_exponential_decay(decay_parameters: list,
     exp_decay = amplitude * np.exp(-decay_rate * (t - np.min(t)))
     conv_exp_decay = np.convolve(exp_decay,irf)
 
-    conv_exp_decay_shifted = irf * 0
-
-    if t_shift > 0:
+    if t_shift >= 0:
         conv_exp_decay_shifted = conv_exp_decay[t_shift : size + t_shift]
     else:
         conv_exp_decay_shifted = np.hstack((conv_exp_decay[t_shift:],conv_exp_decay[0 : size + t_shift]))
 
     conv_exp_decay_shifted = conv_exp_decay_shifted + bgr
 
-    def merit_function(t: np.ndarray,
-                       decay: np.ndarray,
-                       fit: np.ndarray,
-                       method: str):
-            
-            if method == 'MLE':
-                M = np.sum(decay * np.log(fit) + fit) # + np.log(np.fact)
-            elif method == 'LS':
-                M = np.sum((decay - fit)**2)
+    return conv_exp_decay_shifted
 
-            return M
+def merit_function(t: np.ndarray,
+                    decay: np.ndarray,
+                    fit: np.ndarray,
+                    method: str):
+        
+    if method == 'MLE':
+        M = np.sum(-1 * decay * np.log(fit) + fit) # + np.log(np.fact)
+    elif method == 'LS':
+        M = np.sum((decay - fit)**2)
+
+    return M
+
+def fit_exponential_decay(t: np.ndarray,
+                            decay: np.ndarray,
+                            irf: np.ndarray,
+                            initial_guess: tuple,
+                            fitting_method: str = 'MLE'):
     
-    def fit_exponential_decay(t: np.ndarray,
-                              decay: np.ndarray,
-                              irf: np.ndarray,
-                              initial_guess: tuple,
-                              fitting_method: str = 'MLE'):
-         
-         merit_func = lambda decay_parameters : merit_function(
-                        t,
-                        decay,
-                        convoluted_exponential_decay(decay_parameters,t,irf),fitting_method)
-         
-         fit = optimize.minimize(merit_func,
-                           initial_guess,
-                           method = 'Newton-CG')
-         
-         return fit
+    merit_func = lambda decay_parameters : merit_function(
+                t,
+                decay,
+                convoluted_exponential_decay(decay_parameters,t,irf),fitting_method)
+    
+    fit = optimize.minimize(merit_func,
+                    initial_guess,
+                    method = 'BFGS')
+
+    print(fit.message)
+    best_fit_parameters = fit.x
+
+    I_fit = convoluted_exponential_decay(best_fit_parameters,t,irf)
+
+    return fit,I_fit
          
 
 
@@ -68,18 +70,35 @@ def convoluted_exponential_decay(decay_parameters: list,
 
 
 #%%
-t = np.linspace(0,100,100)
+t = np.linspace(0,500,100)
 irf = t*0
-irf[15:20] = 0.2
-exp = convoluted_exponential_decay([1,0.1,2],t,irf,2)
-# fit = fit_exponential_decay(t,)
+irf[15:16] = 1
+irf = irf / np.sum(irf)
+decay_parameters = [500,0.02,5,20]
+exp = convoluted_exponential_decay(decay_parameters,t,irf)
+exp = np.random.poisson(exp)
 
-print(len(exp))
+initial_guess = [100,0.15,4,15]
+initial_fit = convoluted_exponential_decay(initial_guess,t,irf)
+
+M = merit_function(t,exp,initial_fit,'MLE')
+print(M)
+
+fit,final_fit = fit_exponential_decay(t,exp,irf,initial_guess,'MLE')
+print(fit.x)
+print(np.diagonal(fit.hess_inv))
 
 plt.figure()
-plt.plot(irf)
-plt.plot(exp)
+plt.clf()
+plt.plot(irf+decay_parameters[3],label = 'irf')
+plt.plot(exp,label = 'data')
+plt.plot(initial_fit,label='intial fit')
+plt.plot(final_fit,label = 'converged fit')
+plt.legend()
 plt.show()
-# %%
-exp
-# %%
+
+
+
+# %% to do
+# find solution for time shift fit. currently it doesnt fit it. overrule and use value in search range around IRF value
+# ensure that inf values for bgr = 0 do not fail fit
