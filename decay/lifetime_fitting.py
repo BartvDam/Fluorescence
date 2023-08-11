5af22ccb2246429d0c76e58eb90939393ea4cfa0
BartvanDam <b.vdam@protonmai.com>
2023-08-11 16:17:47 +0200

    Timeshift optimization and chi squared implementation

--- decay/lifetime_fitting.py
+++ decay/lifetime_fitting.py
@@ -3,17 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
+from scipy.special import factorial
 
 # %%
 
 def convoluted_exponential_decay(decay_parameters: list,
                                  t: np.ndarray,
-                                 irf: np.ndarray):
+                                 irf: np.ndarray,
+                                 t_shift: int):
 
     amplitude = decay_parameters[0]
     decay_rate = decay_parameters[1]
-    t_shift = int(decay_parameters[2])
-    bgr = decay_parameters[3]
+    # t_shift = int(decay_parameters[2])
+    t_shift = t_shift
+    bgr = decay_parameters[2]
 
     size = len(t)
 
@@ -45,22 +48,47 @@ def fit_exponential_decay(t: np.ndarray,
                             decay: np.ndarray,
                             irf: np.ndarray,
                             initial_guess: tuple,
+                            t_shift_range: tuple,
                             fitting_method: str = 'MLE'):
     
-    merit_func = lambda decay_parameters : merit_function(
-                t,
-                decay,
-                convoluted_exponential_decay(decay_parameters,t,irf),fitting_method)
-    
-    fit = optimize.minimize(merit_func,
-                    initial_guess,
-                    method = 'BFGS')
+    for i,t_shift in enumerate(np.arange(t_shift_range[0],t_shift_range[1]+1)): 
 
+        merit_func = lambda decay_parameters : merit_function(
+                    t,
+                    decay,
+                    convoluted_exponential_decay(decay_parameters,t,irf,t_shift),fitting_method)
+        
+        _fit = optimize.minimize(merit_func,
+                                 initial_guess,
+                                 options={'gtol': 1e-9})
+                                #  method = 'trust-ncg')
+                                #  method = 'BFGS')
+        
+        if (i == 0):
+            fit = _fit
+            best_fit_timeshift = t_shift
+        elif (_fit.fun < fit.fun):
+            fit = _fit
+            best_fit_timeshift = t_shift
+        else:
+            None
+        print(fit.fun)
+        
     print(fit.message)
     best_fit_parameters = fit.x
 
-    I_fit = convoluted_exponential_decay(best_fit_parameters,t,irf)
+    I_fit = convoluted_exponential_decay(best_fit_parameters,t,irf,best_fit_timeshift)
+    I_res = decay - I_fit
 
+    n = len(decay) # number of data points
+    p = len(initial_guess) # number of fitting parameters
+
+    if fitting_method == 'MLE':
+        chi2 = 2*np.sum(I_fit - decay * np.log(I_fit) + np.log(factorial(decay))) # equation 7 Bajzer 1991
+    elif fitting_method == 'LS':
+        chi2 = np.sum((I_res**2)/I_fit) # equation 9 Bajzer 1991. alternatively you could also divide by I_data but you have to deal with the Infs
+    weighted_chi2 = chi2 / (n-p)
+    
     return fit,I_fit
          
 
@@ -70,30 +98,33 @@ def fit_exponential_decay(t: np.ndarray,
 
 
 #%%
-t = np.linspace(0,500,100)
+t = np.linspace(0,500,200)
 irf = t*0
 irf[15:16] = 1
 irf = irf / np.sum(irf)
-decay_parameters = [500,0.02,5,20]
-exp = convoluted_exponential_decay(decay_parameters,t,irf)
+decay_parameters = [500,0.02,20]
+t_shift = 5
+exp = convoluted_exponential_decay(decay_parameters,t,irf,t_shift)
 exp = np.random.poisson(exp)
-
-initial_guess = [100,0.15,4,15]
-initial_fit = convoluted_exponential_decay(initial_guess,t,irf)
+#%%
+initial_guess = [100,0.3,20]
+t_shift_range = [2,7]
+initial_fit = convoluted_exponential_decay(initial_guess,t,irf,t_shift)
 
 M = merit_function(t,exp,initial_fit,'MLE')
-print(M)
+print(fit)
 
-fit,final_fit = fit_exponential_decay(t,exp,irf,initial_guess,'MLE')
+fit,final_fit = fit_exponential_decay(t,exp,irf,initial_guess,t_shift_range,'MLE')
 print(fit.x)
 print(np.diagonal(fit.hess_inv))
 
 plt.figure()
 plt.clf()
-plt.plot(irf+decay_parameters[3],label = 'irf')
-plt.plot(exp,label = 'data')
-plt.plot(initial_fit,label='intial fit')
-plt.plot(final_fit,label = 'converged fit')
+plt.plot(irf+decay_parameters[2],color='gray',label = 'irf')
+plt.plot(exp,color='k',label = 'data')
+plt.plot(initial_fit,color='g',label='intial fit')
+plt.plot(final_fit,color='r',label = 'converged fit')
+plt.yscale('log')
 plt.legend()
 plt.show()
 
